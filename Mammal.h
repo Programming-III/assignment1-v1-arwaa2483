9c47ac1dbffdba61d4403e0a2014efe5e8b29952
arwaa2483 <arwa.mohamed@student.giu-uni.de>
2025-11-12 15:37:28 +0200

    Update Mammal.h

--- Mammal.h
+++ Mammal.h
@@ -1,13 +1,19 @@
-#ifndef MAMMAL_H
-#define MAMMAL_H
-
+#ifndef Mammal_H_INCLUDED
+#define Mammal_H_INCLUDED;
+#include<string>
 #include "Animal.h"
+using namespace std;
 
-//define mammal class here
+class Mammal:public Animal {
+private:
+	string furColor;
+public:
+	Mammal(string name, int age, bool isHungry, string furColor);
+	Mammal();
+	~Mammal();
 
+	string getFurColor();
+	void setFurColor();
 
-
-
-
-
+};
 #endif
