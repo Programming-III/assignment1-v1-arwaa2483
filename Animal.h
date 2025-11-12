6d2be3f98a96bd164ae837b98caf683073afb440
arwaa2483 <arwa.mohamed@student.giu-uni.de>
2025-11-12 15:36:07 +0200

    Update Animal.h

--- Animal.h
+++ Animal.h
@@ -1,11 +1,29 @@
-#ifndef ANIMAL_H
-#define ANIMAL_H
-
-#include <string>
+#ifndef Animal_H_INCLUDED
+#define Animal_H_INCLUDED;
+#include<string>;
 using namespace std;
 
-//define animal class here 
+class Animal {
+protected:
+	string name;
+	int age;
+	bool isHungry;
 
+public:
+	Animal();
+	Animal(string name, int age, bool isHungry);
+	~Animal();
+	void display();
+	void feed();
+	string getName();
+	int getAge();
+	bool getisHungry();
 
+	void setisHungry(bool isHungry);
+	void setName(string name);
+	void setAge(int age);
+	
+	
 
+};
 #endif
